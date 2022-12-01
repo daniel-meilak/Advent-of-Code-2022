871cfb5e19c9fb74f656388484943e17090b7d2b
Daniel Meilak <daniel.mlak@gmail.com>
2022-12-01 23:08:03 +0100

    finished day 1

--- week_1/day_01/day_01.cpp
+++ week_1/day_01/day_01.cpp
@@ -3,20 +3,39 @@
 #include<string>
 #include<algorithm>
 #include<cstdlib>
+#include<climits>
+#include<set>
+#include<iterator>
+#include<functional>
 #include"utils.h"
 
 int main(){
 
     // read input into vector of strings.
-    // std::vector<std::string> delimiters = {};
-    // std::vector<std::string> input = read_input("input", delimiters);
+    std::vector<std::string> delimiters = {};
+    std::vector<std::string> input = read_input("input", delimiters);
 
-    // read input into vector of vector of strings.
-    // std::vector<std::string> delimiters = {};
-    // std::vector<std::vector<std::string>> input = read_input_2D("input", delimiters);
+    int tally{0};
 
-    std::cout << "Answer (part 1): " << std::endl;
-    std::cout << "Answer (part 2): " << std::endl;
+    // sorted calorie totals
+    std::multiset<int, std::greater<int>> calorie_totals;
+
+    // loop through input and tally up each elfs calories
+    for (const auto& cal : input){
+
+        // on empty line, reset tally for next elf
+        if (cal.empty()){
+            calorie_totals.insert(tally);
+            tally   = 0;
+            continue;
+        }
+
+        // add next item to calories tally
+        tally += std::stoi(cal);
+    }
+
+    std::cout << "Answer (part 1): " << *calorie_totals.begin() << std::endl;
+    std::cout << "Answer (part 2): " << *calorie_totals.begin() + *std::next(calorie_totals.begin()) + *std::next(calorie_totals.begin(),2) << std::endl;
 
     return 0;
 }

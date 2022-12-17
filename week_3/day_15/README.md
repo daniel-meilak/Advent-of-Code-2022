f16457c87ee5045e1bd59cc871014ae9124eb806
Daniel Meilak <daniel.mlak@gmail.com>
2022-12-17 17:00:25 +0100

    finished day 15

--- week_3/day_15/README.md
+++ week_3/day_15/README.md
@@ -1,1 +1,3 @@
 ## Day 15
+
+Part 2 is relatively slow due to the brute force approat of iterating from 0 to 4'000'000 and finding the filled intervals. But as the code is very simple, I'm inclined to stick with it until I find a cleaner solution.
--- week_3/day_15/day_15.cpp
+++ week_3/day_15/day_15.cpp
@@ -3,20 +3,60 @@
 #include<string>
 #include<algorithm>
 #include<cstdlib>
+#include<unordered_set>
+#include<map>
 #include"utils.h"
+#include"point.h"
+#include"interval_map.h"
+
+// forward function declaration
+interval_map_t<long> find_intervals(const std::vector<std::vector<int>>& input, long yLevel);
 
 int main(){
 
-    // read input into vector of strings.
-    // std::vector<std::string> delimiters = {};
-    // std::vector<std::string> input = read_input("input", delimiters);
-
     // read input into vector of vector of strings.
-    // std::vector<std::string> delimiters = {};
-    // std::vector<std::vector<std::string>> input = read_input_2D("input", delimiters);
+    std::vector<std::string> delimiters = {"Sensor at x=", ", y=", ": closest beacon is at x="};
+    std::vector<std::vector<int>> input = input_to_int_2D(read_input_2D("input", delimiters));
 
-    std::cout << "Answer (part 1): " << std::endl;
-    std::cout << "Answer (part 2): " << std::endl;
+    interval_map_t<long> interval = find_intervals(input, 2'000'000);
+ 
+    long part1 = interval.begin()->second - interval.begin()->first;
+
+    long part2;
+    for (long i=0; i<=4'000'000; i++){
+        interval = find_intervals(input, i);
+
+        if (interval.size() >= 2){
+            part2 = i + (interval.begin()->second + 1)*4'000'000;
+            break;
+        }
+    }
+
+    std::cout << "Answer (part 1): " << part1 << std::endl;
+    std::cout << "Answer (part 2): " << part2 << std::endl;
 
     return 0;
+}
+
+interval_map_t<long> find_intervals(const std::vector<std::vector<int>>& input, long yLevel){
+
+    interval_map_t<long> intervals;
+
+    for (const auto& line : input){
+
+        long x = line[0], y = line[1];
+
+        // radius of source to beacon
+        long radius  = manhattan(line[0],line[1],line[2], line[3]);
+
+        // distance to y = 2'000'000
+        long distance = std::abs(y - yLevel);
+
+        long diff = distance - radius;
+
+        if ( diff < 0){ intervals.insert(x+diff, x-diff); }
+    }
+
+    return intervals;
+
 }
